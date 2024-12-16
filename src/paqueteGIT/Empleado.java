3a54a196b7e8e7d31bf0a7c4a1e11c869a8506f9
David <david.alvarez06@somo.eus>
2024-12-16 08:51:36 +0100

    Añadido constructor completo

--- src/paqueteGIT/Empleado.java
+++ src/paqueteGIT/Empleado.java
@@ -7,4 +7,14 @@ public class Empleado {
 	private byte edad;
 	private float sueldo;
 	private boolean hijos; 
+	
+public Empleado(String dni, String nombre, String apellidos, byte edad, float sueldo, boolean hijos) {
+	super();
+	this.dni=dni;
+	this.nombre=nombre;
+	this.apellidos=apellidos;
+	this.edad=edad;
+	this.sueldo=sueldo;
+	this.hijos=hijos;
+	}
 }
